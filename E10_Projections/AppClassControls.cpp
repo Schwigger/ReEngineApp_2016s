7376ea5d07ffc9b4b7b771a743a18174f27f6cb8
MAIN\mxs9629 <schweigardt96>
2017-03-21 12:14:44 -0400

    Started E10

--- E10_Projections/AppClassControls.cpp
+++ E10_Projections/AppClassControls.cpp
@@ -7,8 +7,8 @@ void AppClass::ProcessKeyboard(void)
 	//ON PRESS/RELEASE
 #pragma region ON PRESS/RELEASE DEFINITION
 	static bool	bLastF1 = false, bLastF2 = false, bLastF3 = false, bLastF4 = false, bLastF5 = false,
-				bLastF6 = false, bLastF7 = false, bLastF8 = false, bLastF9 = false, bLastF10 = false,
-				bLastEscape = false;
+		bLastF6 = false, bLastF7 = false, bLastF8 = false, bLastF9 = false, bLastF10 = false,
+		bLastEscape = false;
 #define ON_KEY_PRESS_RELEASE(key, pressed_action, released_action){  \
 			bool pressed = sf::Keyboard::isKeyPressed(sf::Keyboard::key);			\
 			if(pressed){											\
@@ -27,21 +27,29 @@ void AppClass::ProcessKeyboard(void)
 	//F2 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F2))
 	{
+		m_m4Projection = glm::perspective(45.0f, 1080.0f / 768.0f, 0.01f, 1000.0f);
+		m_m4View = glm::lookAt(glm::vec3(15.0f, 0.0f, 0.0f), glm::vec3(14.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	}
 
 	//F3 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F3))
 	{
+		m_m4Projection = glm::perspective(45.0f, 1080.0f / 768.0f, 0.01f, 15.0f);
+		m_m4View = glm::lookAt(glm::vec3(0.0f, 0.0f, 15.0f), glm::vec3(0.0f, 0.0f, 14.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
 	//F4 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F4))
 	{
+		m_m4Projection = glm::perspective(45.0f, 1080.0f / 768.0f, 0.01f, 1000.0f);
+		m_m4View = glm::lookAt(glm::vec3(15.0f, 0.0f, 0.0f), glm::vec3(14.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
 	//F5 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F5))
 	{
+		m_m4Projection = glm::ortho(-15.0f, 15.0f, -1.0f, 1.0f, 0.01f, 1000.0f);
+		m_m4View = glm::lookAt(glm::vec3(15.0f, 0.0f, 0.0f), glm::vec3(14.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
 	//Exit the program
