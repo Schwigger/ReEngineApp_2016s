185a2f8385ce5144059f3305c5d3abf83b24ddd6
Schwigger <schweigardt96@gmail.com>
2017-03-22 20:45:23 -0400

    E10_Projections Completed

--- E10_Projections/AppClassControls.cpp
+++ E10_Projections/AppClassControls.cpp
@@ -48,7 +48,7 @@ void AppClass::ProcessKeyboard(void)
 	//F5 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F5))
 	{
-		m_m4Projection = glm::ortho(-15.0f, 15.0f, -1.0f, 1.0f, 0.01f, 1000.0f);
+		m_m4Projection = glm::ortho(-15.0f, 15.0f, -45.0f, 45.0f, 0.01f, 1000.0f);
 		m_m4View = glm::lookAt(glm::vec3(15.0f, 0.0f, 0.0f), glm::vec3(14.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
