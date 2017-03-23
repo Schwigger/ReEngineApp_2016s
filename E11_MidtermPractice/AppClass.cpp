6208c3aa06914021609b686e5f38acdd8cc5a402
MAIN\mxs9629 <schweigardt96>
2017-03-23 12:17:01 -0400

    E11_MidtermPractice started

--- E11_MidtermPractice/AppClass.cpp
+++ E11_MidtermPractice/AppClass.cpp
@@ -16,11 +16,37 @@ void AppClass::Update(void)
 #pragma endregion
 #pragma region YOUR CODE GOES HERE
 	m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
+
+	static DWORD startTimeSystem = GetTickCount();	//Ask system's start up time
+	DWORD timeApplication = GetTickCount() - startTimeSystem;	//Get current time and subtract the start time
+	float timer = timeApplication / 1000.0f;	//time is in milliseconds need it in seconds
+	float timerMapped;
+	static float top = 1.0f;
+	static float bottom = 0.0f;
+	static float limit = 5.0f;
+	vector3 v3Lerp;
+	vector3 destination = vector3(0.0f, 5.0f, 0.0f);
+	vector3 home;
+
+	timerMapped = MapValue(timer, 0.0f, limit, bottom, top);
+
+	if (timerMapped > 1.0f) {
+		startTimeSystem = GetTickCount();
+
+		float temp = top;
+		top = bottom;
+		bottom = temp;
+	}
+	
+	v3Lerp = glm::lerp(v3Lerp, destination, timerMapped);
+
+	m_m4Steve = glm::translate(v3Lerp);
+
 #pragma endregion
 #pragma region DOES NOT NEED CHANGES
 	//Set the model matrix
 	m_pMeshMngr->SetModelMatrix(m_m4Steve, "Steve");
-	
+
 	//Adds all loaded instance to the render list
 	m_pMeshMngr->AddInstanceToRenderList("ALL");
 
@@ -32,5 +58,7 @@ void AppClass::Update(void)
 	m_pMeshMngr->PrintLine(std::to_string(dTotalTime), RERED);
 	m_pMeshMngr->Print("FPS:");
 	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
+
+	m_pMeshMngr->Print(std::to_string(top));
 #pragma endregion
 }
