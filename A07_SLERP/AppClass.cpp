1b610e2f6555671e5495765d88b1bf35aa487a8a
Schwigger <schweigardt96@gmail.com>
2017-03-25 17:17:25 -0400

    Progress on A07_SLERP

--- A07_SLERP/AppClass.cpp
+++ A07_SLERP/AppClass.cpp
@@ -1,7 +1,7 @@
 #include "AppClass.h"
 void AppClass::InitWindow(String a_sWindowName)
 {
-	super::InitWindow("SLERP - YOUR USER NAME GOES HERE"); // Window Name
+	super::InitWindow("SLERP - mxs9629"); // Window Name
 
 	//Setting the color to black
 	m_v4ClearColor = vector4(0.0f);
@@ -36,6 +36,9 @@ void AppClass::Update(void)
 	if (m_bFPC == true)
 		CameraRotation();
 
+	ArcBall();
+	m_pMeshMngr->SetModelMatrix(ToMatrix4(m_qArcBall), 0);
+
 	//Getting the time between calls
 	double fCallTime = m_pSystem->LapClock();
 	//Counting the cumulative time
@@ -43,14 +46,54 @@ void AppClass::Update(void)
 	fRunTime += fCallTime;
 
 	//Earth Orbit
-	double fEarthHalfOrbTime = 182.5f * m_fDay; //Earths orbit around the sun lasts 365 days / half the time for 2 stops
+	float fEarthHalfOrbTime = 182.5f * m_fDay; //Earths orbit around the sun lasts 365 days / half the time for 2 stops
 	float fEarthHalfRevTime = 0.5f * m_fDay; // Move for Half a day
 	float fMoonHalfOrbTime = 14.0f * m_fDay; //Moon's orbit is 28 earth days, so half the time for half a route
 
+	//Matrices for transformations
+	matrix4 m_m4Sun = IDENTITY_M4;
+	matrix4 m_m4Earth = IDENTITY_M4;
+	matrix4 m_m4Moon = IDENTITY_M4;
+
+	//Matrices to hold relative translations
+	matrix4 distEarth = glm::translate(11.0f, 0.0f, 0.0f);
+	matrix4 distMoon = glm::translate(2.0f, 0.0f, 0.0f);
+
+	//Matrices to hold relative scales
+	matrix4 scaleEarth = glm::scale(vector3(0.524f));
+	matrix4 scaleMoon = glm::scale(vector3(0.27f)) * scaleEarth;
+	matrix4 scaleSun = glm::scale(vector3(5.936f));
+
+	//Quaternions for rotation
+	glm::quat q1 = glm::angleAxis(0.0f, vector3(0.0f, 0.0f, 1.0f));
+	glm::quat q2 = glm::angleAxis(359.0f, vector3(0.0f, 0.0f, 1.0f));
+
+	glm::quat revEarth;
+	glm::quat orbEarth;
+	glm::quat rotMoon;
+	glm::quat orbMoon;
+
+	float fPercentEarthRev = MapValue((float)fRunTime, 0.0f, 2*fEarthHalfRevTime, 0.0f, 1.0f);
+	revEarth = glm::mix(q1, q2, fPercentEarthRev);
+
+	matrix4 earthRevolution = ToMatrix4(revEarth);
+
+	float fPercentEarthOrb = MapValue((float)fRunTime, 0.0f, 2*fEarthHalfOrbTime, 0.0f, 1.0f);
+	//orbEarth = glm::mix(q1, q2, fPercentEarthOrb);
+
+	float fPercentMoonOrb = MapValue((float)fRunTime, 0.0f, 2 * fMoonHalfOrbTime, 0.0f, 1.0f);
+	orbMoon = glm::mix(q1, q2, fPercentMoonOrb);
+
+	matrix4 moonOrbit = ToMatrix4(orbMoon);
+
+	m_m4Earth = m_m4Sun * ToMatrix4(orbEarth) * distEarth;
+	m_m4Moon = m_m4Earth * moonOrbit * distMoon * scaleMoon;
+	m_m4Sun = m_m4Sun * scaleSun;
+
 	//Setting the matrices
-	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Sun");
-	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Earth");
-	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Moon");
+	m_pMeshMngr->SetModelMatrix(m_m4Sun, "Sun");
+	m_pMeshMngr->SetModelMatrix(m_m4Earth, "Earth");
+	m_pMeshMngr->SetModelMatrix(m_m4Moon, "Moon");
 
 	//Adds all loaded instance to the render list
 	m_pMeshMngr->AddInstanceToRenderList("ALL");
@@ -64,7 +107,7 @@ void AppClass::Update(void)
 
 	//Print info on the screen
 	m_pMeshMngr->PrintLine(m_pSystem->GetAppName(), REYELLOW);
-	
+
 	m_pMeshMngr->Print("Time:");
 	m_pMeshMngr->PrintLine(std::to_string(fRunTime));
 
@@ -104,7 +147,7 @@ void AppClass::Display(void)
 		m_pMeshMngr->AddGridToRenderList(1.0f, REAXIS::XY, REBLUE * 0.75f); //renders the XY grid with a 100% scale
 		break;
 	}
-	
+
 	m_pMeshMngr->Render(); //renders the render list
 
 	m_pGLSystem->GLSwapBuffers(); //Swaps the OpenGL buffers
