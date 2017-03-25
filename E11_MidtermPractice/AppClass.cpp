206630973f0aa5471909f42a626e2911b2843dcb
Schwigger <schweigardt96@gmail.com>
2017-03-25 15:27:16 -0400

    E11_Midterm Practice - Completed

--- E11_MidtermPractice/AppClass.cpp
+++ E11_MidtermPractice/AppClass.cpp
@@ -10,38 +10,36 @@ void AppClass::Update(void)
 #pragma endregion
 #pragma region FEEL FREE TO USE THIS CLOCK
 	//Calculate delta and total times
-	static double dTotalTime = 0.0; //Total time of the simulation
-	double dDeltaTime = m_pSystem->LapClock(); //time difference between function calls
+	static float dTotalTime = 0.0f; //Total time of the simulation
+	float dDeltaTime = m_pSystem->LapClock(); //time difference between function calls
 	dTotalTime += dDeltaTime; //Incrementing the time differences 
 #pragma endregion
 #pragma region YOUR CODE GOES HERE
 	m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
 
-	static DWORD startTimeSystem = GetTickCount();	//Ask system's start up time
-	DWORD timeApplication = GetTickCount() - startTimeSystem;	//Get current time and subtract the start time
-	float timer = timeApplication / 1000.0f;	//time is in milliseconds need it in seconds
 	float timerMapped;
-	static float top = 1.0f;
-	static float bottom = 0.0f;
+	static float rotation = 0.0f;
+	static float top = 1.0;
+	static float bottom = 0.0;
 	static float limit = 5.0f;
 	vector3 v3Lerp;
-	vector3 destination = vector3(0.0f, 5.0f, 0.0f);
-	vector3 home;
+	vector3 away = vector3(0.0f, 5.0f, 0.0f);
 
-	timerMapped = MapValue(timer, 0.0f, limit, bottom, top);
+	timerMapped = MapValue(dTotalTime, 0.0f, limit, bottom, top);
 
-	if (timerMapped > 1.0f) {
-		startTimeSystem = GetTickCount();
+	if (timerMapped > 1.0f || timerMapped < 0.0f) {
+		dTotalTime = 0.0;
 
 		float temp = top;
 		top = bottom;
 		bottom = temp;
 	}
-	
-	v3Lerp = glm::lerp(v3Lerp, destination, timerMapped);
 
-	m_m4Steve = glm::translate(v3Lerp);
 
+	v3Lerp = glm::lerp(v3Lerp, away, timerMapped);
+
+	m_m4Steve = glm::rotate(IDENTITY_M4, glm::degrees(rotation), vector3(0.0f, 0.0f, 1.0f)) * glm::translate(v3Lerp);
+	rotation += dDeltaTime * 2.5f;
 #pragma endregion
 #pragma region DOES NOT NEED CHANGES
 	//Set the model matrix
@@ -53,12 +51,10 @@ void AppClass::Update(void)
 	//Indicate the FPS
 	int nFPS = m_pSystem->GetFPS();
 	//Print info on the screen
-	m_pMeshMngr->PrintLine("\n" + m_pSystem->GetAppName(), REYELLOW);
+	m_pMeshMngr->PrintLine("\n" + m_pSystem->GetAppName() + " - Schweigardt", REYELLOW);
 	m_pMeshMngr->Print("Seconds:");
 	m_pMeshMngr->PrintLine(std::to_string(dTotalTime), RERED);
 	m_pMeshMngr->Print("FPS:");
 	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
-
-	m_pMeshMngr->Print(std::to_string(top));
 #pragma endregion
 }
