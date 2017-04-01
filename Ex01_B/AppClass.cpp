e92c315f13b6ecfb55f599342c253246fa3f3f8f
Schwigger <schweigardt96@gmail.com>
2017-04-01 19:35:55 -0400

    Extra Credit Practical Completed

--- Ex01_B/AppClass.cpp
+++ Ex01_B/AppClass.cpp
@@ -15,6 +15,36 @@ void AppClass::Update(void)
 
 #pragma region YOUR CODE GOES HERE
 	modelMatrix = IDENTITY_M4;
+
+	float fPercentTrans;
+	float fPercentRot;
+	static float fTop = 1.0;
+	static float fBottom = 0.0f;
+
+	matrix4 m4Translate;
+	vector3 v3Lerp;
+
+	quaternion q1 = glm::angleAxis(0.0f, vector3(0.0f, 0.0f, 1.0f));
+	quaternion q2 = glm::angleAxis(359.0f, vector3(0.0f, 0.0f, 1.0f));
+	quaternion q3;
+
+	fPercentTrans = MapValue(fTimer, 0.0f, 2.5f, fBottom, fTop);
+	fPercentRot = MapValue(fTimer, 0.0f, 2.5f, 0.0f, 1.0f);
+
+	if (fPercentTrans > 1.0f || fPercentTrans < 0.0f) {
+		fTimer = 0.0f;
+
+		float fTemp = fTop;
+		fTop = fBottom;
+		fBottom = fTemp;
+	}
+
+	q3 = glm::mix(q1, q2, fPercentRot);
+
+	v3Lerp = glm::lerp(v3Lerp, vector3(0.0f,3.0f,0.0f), fPercentTrans);
+	m4Translate = glm::translate(v3Lerp);
+
+	modelMatrix *= (ToMatrix4(q3) * m4Translate);
 #pragma endregion
 
 #pragma region DOES NOT NEED CHANGES
